1767efefc077d08474d96ff054085efa68e90475
Dimitra Mavroforaki <dimitra.mavroforaki@twi.gr>
2024-09-09 14:10:41 +0300

    add speech_dataset

--- main.py
+++ main.py
@@ -4,17 +4,13 @@
 Created on Fri Sep  6 16:06:37 2024
 
 @author: dimitra
+
+@article{reddy2019scalable,
+  title={A Scalable Noisy Speech Dataset and Online Subjective Test Framework},
+  author={Reddy, Chandan KA and Beyrami, Ebrahim and Pool, Jamie and Cutler, Ross and Srinivasan, Sriram and Gehrke, Johannes},
+  journal={Proc. Interspeech 2019},
+  pages={1816--1820},
+  year={2019}
+}
 """
-from azureml.opendatasets import MsSnsd
-import os
 
-# Specify the download folder for the dataset
-download_path = "./ms_snsd_sample"
-if not os.path.exists(download_path):
-    os.makedirs(download_path)
-
-# Load the dataset
-ms_snsd = MsSnsd()
-
-# Download a sample of the data (e.g., 10 files)
-ms_snsd.download(download_path, max_files=10)
