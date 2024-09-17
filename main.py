88bc779012b234fc84a37bcb59ab03e3811a716e
Dimitra Mavroforaki <dimitra.mavroforaki@twi.gr>
2024-09-17 11:34:40 +0300

    svm+mlp classifier

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ Created on Fri Sep  6 16:06:37 2024
 
 from pathlib import Path
 from src.data_manipulation import create_dataset,combine_datasets
+from src.classifiers import train_svm,train_mlp,evaluate
 from sklearn.model_selection import train_test_split
 
 
@@ -24,6 +25,13 @@ if __name__=="__main__":
     X_noise_data, Y_noise_data = create_dataset(Path('data/Noise_training/'),0)
     X,Y = combine_datasets(X_clean_data, Y_clean_data, X_noise_data, Y_noise_data)
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
-    print(X_train.shape,Y_train.shape)
-    print(X_test.shape, Y_test.shape)
+    
+    #SVM
+    svm = train_svm(X_train, Y_train)
+    evaluate( X_test, Y_test,svm,"SVM")
+        
+    #MLP
+    mlp = train_mlp(X_train, Y_train)
+    evaluate(X_test,Y_test,mlp,"MLP")
+    
 
---
+++ src/classifiers.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 16 21:17:35 2024
+
+@author: dimitra
+"""
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
+from sklearn import metrics
+
+
+def train_svm(X_train, Y_train):
+    #rbf kernel is used for complex data with no clear decision boundaries
+    svm = SVC(kernel='rbf')
+    svm.fit(X_train, Y_train)
+    return svm
+
+def train_mlp(X_train,Y_train):
+    mlp = MLPClassifier(hidden_layer_sizes=(150,100,50), max_iter=300,activation = 'relu',solver='adam',random_state=1)
+    mlp.fit(X_train, Y_train)
+    return mlp
+
+def evaluate(X_test,Y_test,classifier,label):
+    Y_pred = classifier.predict(X_test)
+    # Model Accuracy: how often is the classifier correct?
+    print(f'{label} Accuracy:{metrics.accuracy_score(Y_test, Y_pred)}')
+    # Model Precision: what percentage of positive tuples are labeled as such?
+    print(f'{label} Precision:{metrics.precision_score(Y_test, Y_pred)}')
+    # Model Recall: what percentage of positive tuples are labelled as such?
+    print(f'{label} Recall:{metrics.recall_score(Y_test, Y_pred)}')
