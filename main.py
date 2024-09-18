51321cef33a7f8755fa2b3d2b81caee186dea68e
ThanosArapakis1997 <it21603@hua.gr>
2024-09-18 15:26:08 +0300

    minor

--- main.py
+++ main.py
@@ -16,34 +16,61 @@ Created on Fri Sep  6 16:06:37 2024
 
 from pathlib import Path
 from src.data_manipulation import create_dataset,combine_datasets,convert_to_mel
-from src.classifiers import train_svm,train_mlp,evaluate
+from src.classifiers import train_svm,train_mlp,train_rnn,evaluate,rnn_evaluate
 from sklearn.model_selection import train_test_split
 from scipy.signal import medfilt
 from src.config import SAMPLING_RATE,FILTER_SIZE
 from audio import extract_clean_speech_intervals,extract_clean_speech
 import librosa
 import soundfile as sf
+import torch
 
+def rnn_predict(mel_noisyspeech):
+    data_tensor = torch.from_numpy(mel_noisyspeech)
+    data_tensor = data_tensor.unsqueeze(0)
+    predictions = rnn(data_tensor)
+    predicted_classes = (predictions > 0.5).float()
+    # rounded_predictions = medfilt(predicted_classes, kernel_size=FILTER_SIZE)
+    return predicted_classes
 
 if __name__=="__main__":
-    X_clean_data, Y_clean_data = create_dataset(Path('data/CleanSpeech_training/'),1)
-    X_noise_data, Y_noise_data = create_dataset(Path('data/Noise_training/'),0)
+    X_clean_data, Y_clean_data = create_dataset(Path('speech_dataset/data/CleanSpeech_training/'),1)
+    X_noise_data, Y_noise_data = create_dataset(Path('speech_dataset/data/Noise_training/'),0)
     X,Y = combine_datasets(X_clean_data, Y_clean_data, X_noise_data, Y_noise_data)
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
-    
-    #SVM
-    svm = train_svm(X_train, Y_train)
-    evaluate( X_test, Y_test,svm,"SVM")
-        
-    #MLP
-    mlp = train_mlp(X_train, Y_train)
-    evaluate(X_test,Y_test,mlp,"MLP")
-    
 
-    audio,_ = librosa.load('data/NoisySpeech_training/noisy1_SNRdb_0.0_clnsp1.wav', sr=SAMPLING_RATE)
-    mel_noisyspeech = convert_to_mel(audio)
-    predictions = svm.predict(mel_noisyspeech)
-    predictions = medfilt(predictions, kernel_size=FILTER_SIZE) 
-    cleanspeech_intervals = extract_clean_speech_intervals(audio,predictions)
-    cleanspeech = extract_clean_speech(cleanspeech_intervals,audio)
-    sf.write('reconstructed_speech.wav', cleanspeech, SAMPLING_RATE)
+    print("Επιλέξτε μοντέλο: ")
+    model= input()
+    
+    if(model =='1'):
+        #SVM
+        svm = train_svm(X_train, Y_train)
+        # evaluate( X_test, Y_test,svm,"SVM")
+    
+    if(model == '2'):
+        # #MLP
+        mlp = train_mlp(X_train, Y_train)
+        evaluate(X_test,Y_test,mlp,"MLP")
+    
+    if(model == '3'):
+        #RNN
+        rnn = train_rnn(X_train, Y_train)
+        rnn_evaluate(X_test,Y_test, rnn)
+
+    if(model != '3'):
+        audio,_ = librosa.load('speech_dataset/data/NoisySpeech_training/noisy2_SNRdb_0.0_clnsp2.wav', sr=SAMPLING_RATE)
+        mel_noisyspeech = convert_to_mel(audio)
+        predictions = svm.predict(mel_noisyspeech)
+        predictions = medfilt(predictions, kernel_size=FILTER_SIZE) 
+        cleanspeech_intervals = extract_clean_speech_intervals(audio,predictions)
+        cleanspeech = extract_clean_speech(cleanspeech_intervals,audio)
+        sf.write('reconstructed_speech_rnn.wav', cleanspeech, SAMPLING_RATE)
+    else:
+        audio,_ = librosa.load('speech_dataset/data/NoisySpeech_training/noisy10_SNRdb_0.0_clnsp10.wav', sr=SAMPLING_RATE)
+        mel_noisyspeech = convert_to_mel(audio)
+        predictions= rnn_predict(mel_noisyspeech)
+        # predictions = medfilt(predictions, kernel_size=FILTER_SIZE) 
+        cleanspeech_intervals = extract_clean_speech_intervals(audio,predictions)
+        cleanspeech = extract_clean_speech(cleanspeech_intervals,audio)
+        sf.write('reconstructed_speech_rnn.wav', cleanspeech, SAMPLING_RATE)
+
